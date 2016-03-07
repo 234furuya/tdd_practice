f400fcf7116330219fb861b6d70e31f7194d9fcc
234furuya <ryoko_furuya@kogasoftware.com>
2016-03-07 15:42:34 +0900

    Queue.add Error test

--- spec/lib/queue_spec.rb
+++ spec/lib/queue_spec.rb
@@ -7,5 +7,9 @@ describe 'Queue Class' do
     it 'should add an item to queue' do
       expect{ queue.add(1) }.to change(queue.list, :length).by(1)
     end
+
+    it 'should raise Error if add more than 5 items' do
+      expect{ 6.times{|index| queue.add(index) } }.to raise_error(Exception)
+    end
   end
 end
