62f11d887785ec16632320ba60a04016d4a4513a
234furuya <ryoko_furuya@kogasoftware.com>
2016-03-07 15:28:48 +0900

    Queue.add test

--- spec/lib/queue_spec.rb
+++ spec/lib/queue_spec.rb
@@ -5,7 +5,7 @@ describe 'Queue Class' do
   let(:queue) { Queue.new }
   describe '.add' do
     it 'should add an item to queue' do
-      expect(queue.add).to be true
+      expect{ queue.add }.to change(queue.list, :length).by(1)
     end
   end
 end
